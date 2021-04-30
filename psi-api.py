4081ed97f43c161fb2574e86f745113904170ac3
Seoseb <79011155+seoseb-de@users.noreply.github.com>
2021-05-01 01:11:52 +0200

    Updated to streamlit 0.81
    Updated to Streamlit 0.81 and integrated the new `st.form()` in order to group configuration and input.
    Use `streamlit.beta_expander()` to show screenshot of GSC export

--- psi-api.py
+++ psi-api.py
@@ -1,44 +1,40 @@
 """
 @seoseb
-test project for streamlit
-Import a GSC Error Report for URLs with a bad CLS and get a table ob elements
+first  project with streamlit
+Import a GSC Error Report for URLs with a bad CLS and get a table of elements
 that contribute to each URL's CLS
 
-Script calls PSI API for each URL in upload file and extracts CLS Elements
-from API Response
+Script calls PSI API for each URL in uploaded file and retrieves CLS Elements
+from API Response, puts them into a dataframe which can be downloaded at the end.
 
 inspired and motivated by Charly Wargnier @DataChaz
 """
 
-# imports
+###########
+# imports #
+###########
+
 
 import streamlit as st
 import requests
 import base64
 import pandas as pd
 
-# variables
+
+#############
+# variables #
+#############
+
 
 request_url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed'
-
-# request parameters
-# Params = {"key":psi_api_key, "url": singleUrlToCheck, "strategy": Device, "category": TestArt, "locale" : Sprache}
-
-# list of URLs to check
 urls_to_check = []
-
-# lists for collecting results
 cls_items = []
 
 
-# lcpItems = []
-# imageEncodingItems = []
-# responsiveImagesItems = []
+########################################################
+# function for calling API and extracting CLS Elements #
+########################################################
 
-
-###
-# function for calling API and extracting CLS Elements
-###
 
 def run_check(url_list):
     run = 0
@@ -91,6 +87,7 @@ def run_check(url_list):
                                   'NA'
                                   ])
         else:
+            # todo: add handler for missing API key exception
             st.error('Calling API for ' + single_url_to_check + ' failed :(')
 
         # update the progress bar with each iteration.
@@ -103,9 +100,10 @@ def run_check(url_list):
 
     base64_data_frame = base64.b64encode(cls_elements_export.to_csv(index=False).encode()).decode()
 
-    st.subheader('have a quick look at the data')
+    st.subheader('3. Have a quick look at the data')
     st.write(cls_elements_export)
 
+    st.subheader('4. Export table')
     href = f'<a href="data:file/csv;base64,{base64_data_frame}" download="Mapping_to_URL.csv">↴ Download Table</a>'
     st.markdown(href, unsafe_allow_html=True)
     st.markdown('_Save, inspect and fix those shifts :) _')
@@ -113,27 +111,34 @@ def run_check(url_list):
     return cls_items
 
 
-###
+###########################
+# Title for Streamlit App #
+###########################
 
-# Title for Streamlit App
-st.image('https://www.seoseb.de/img/seoseb_icon_x96.png')
-st.title('PSI-API Extractor for CLS Elements')
 
+st.title('![@seoseb](https://www.seoseb.de/img/seoseb_icon_x48.png) PSI-API Extractor for CLS Elements')
 st.markdown('Check which elements are shifted during page load and how much they contribute to CLS of a given URL'
-            ' by uploading a CLS Error Report from Google Search Console')
+            ' by uploading a CLS Error Report from Google Search Console.')
+
+st.info('You\'ll need an PSI API key: '
+        '[Get yours here.](https://developers.google.com/speed/docs/insights/v5/get-started#key)')
+
+with st.beta_expander('This is where you find your export files in Google Search Console'):
+    st.markdown('Go to Core Web Vitals Report in Google Search Console. '
+                'Chose the device type you\'re interested in and then go to \"CLS Issues\".')
+    st.image('gsc-export-screen.png', caption='To download the report data choose: Export > Download CSV')
+
 st.markdown('---')
 
-st.subheader('Enter your API key*:')
+st.subheader('1. Setup')
 
-psi_api_key = st.text_input('Your PSI API Key *will not be saved nor transferred elsewhere',
-                            '')
-
-
-st.subheader('Upload your Search Console CLS error export table')
-st.image('gsc-export-screen.png', caption='This is where you find the file you need: choose Export > Download CSV')
-
-# upload GSC export file
-uploaded_file = st.file_uploader('Upload your downloaded csv file here:')
+# Form element collecting all the necessary configurations
+with st.form(key='my_form'):
+    psi_api_key = st.text_input('Your PSI API Key will not be saved nor transferred elsewhere', 'API-KEY')
+    device = st.radio('select device type to check pagespeed on: ', ('Mobile', 'Desktop'))
+    # upload GSC export file
+    uploaded_file = st.file_uploader('Upload your downloaded csv file here:')
+    submit_button = st.form_submit_button(label='Submit')
 
 if uploaded_file is not None:
 
@@ -147,14 +152,20 @@ if uploaded_file is not None:
 
     # set request variables
 
-    device = st.radio('select device type to check pagespeed on: ', ('Mobile', 'Desktop'))
     test_type = 'PERFORMANCE'
     locale = 'de-DE'
 
-    if st.button('Check for CLS Elements'):
+    st.subheader('2. Fetch CLS elements')
+
+    if st.button('▶️ Check for CLS Elements'):
         run_check(urls_to_check)
     else:
         st.write('waiting for your go!')
+
+
+##########
+# footer #
+##########
 
 st.markdown('---')
 st.markdown('_fiddled by [seoseb](https://www.seoseb.de/) | [@seoseb](https://twitter.com/seoseb)_')
